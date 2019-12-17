649d5c0aba3943a087a3ce7125685622896f582c
RachithP <rachithprakash@gmail.com>
2019-12-17 11:12:45 -0500

    adding plots for hyperparams and individual losses

--- plot_loss.py
+++ plot_loss.py
@@ -18,14 +18,41 @@ def _main():
 
     train_loss = log['train_loss']
     val_loss = log['val_loss']
-    dense_1_loss = log['dense_1_loss']
-    activation_1_loss = log['activation_1_loss']
+
+    try:
+        dense_1_loss = log['dense_1_loss']
+        activation_1_loss = log['activation_1_loss']
+    except ValueError:
+        dense_1_loss = np.zeros((train_loss.shape[0],1))
+        activation_1_loss = np.zeros((train_loss.shape[0],1))
+        print("There is no logged value for separate losses :(")
+
+    try:
+        beta = log['beta']
+        alpha = log['alpha']
+    except ValueError:
+        beta = np.zeros((train_loss.shape[0],1))
+        alpha = np.zeros((train_loss.shape[0],1))
+        print("There is no logged values for beta, alpha :(")
+    
+    try:
+        lr = log['learning_rate']
+    except ValueError:
+        lr = np.zeros((train_loss.shape[0],1))
+        print("There is no logged value for learning_rate :(")
+    
     timesteps = list(range(train_loss.shape[0]))
     
     # Plot losses
+    plt.subplot(121)
     plt.plot(timesteps, train_loss, 'r-', timesteps, val_loss, 'b-', timesteps, dense_1_loss, 'k-', timesteps, activation_1_loss, 'g-')
     plt.legend(["Training loss", "Validation loss", "Regression loss", "classification loss"])
     plt.ylabel('Losses')
+    plt.xlabel('Epochs')
+    plt.subplot(122)
+    plt.plot(timesteps, beta, 'r-', timesteps, alpha, 'b-', timesteps, lr*1000, 'g-')
+    plt.legend(["model-weight: beta", "model_weight: alpha", "learning rate*1000"])
+    plt.ylabel('hyper_parameters')
     plt.xlabel('Epochs')
     plt.show()
     plt.savefig(os.path.join(FLAGS.experiment_rootdir, "log.png"))
